0a661d0771d652fb1f9d27509708a1695bd16542
liangruirui <654453664@qq.com>
2021-01-21 13:37:53 +0800

    优化

--- app/src/main/java/com/lihui/hilt/di/interceptor/RequestInterceptor.kt
+++ app/src/main/java/com/lihui/hilt/di/interceptor/RequestInterceptor.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import com.lihui.hilt.BuildConfig
 import com.lihui.hilt.data.ds.DataStoreValue
 import com.lihui.hilt.data.ds.DsUtil
+import com.lihui.hilt.uitl.CacheManager
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -13,7 +14,7 @@ class RequestInterceptor: Interceptor {
             .newBuilder()
             .addHeader("version", BuildConfig.VERSION_NAME)
             .addHeader("model", Build.MODEL)
-            .addHeader(DataStoreValue.TOKEN, DsUtil.getToken())
+            .addHeader(DataStoreValue.TOKEN, CacheManager.instance.getToken())
             .build()
       return  chain.proceed(request)
     }
