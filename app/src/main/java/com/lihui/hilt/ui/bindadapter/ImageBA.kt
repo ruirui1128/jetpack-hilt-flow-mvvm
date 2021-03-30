3369df5de613ac1d8d973aa6ac9d8721273bfe5d
liangruirui <654453664@qq.com>
2021-03-30 16:46:38 +0800

    新增用户界面信息,丰富实现方式

--- app/src/main/java/com/lihui/hilt/ui/bindadapter/ImageBA.kt
+++ app/src/main/java/com/lihui/hilt/ui/bindadapter/ImageBA.kt
@@ -2,7 +2,12 @@ package com.lihui.hilt.ui.bindadapter
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.Observer
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import com.lihui.hilt.enums.StarLevelEnum
+import com.lihui.hilt.ui.vm.UserVm
+import com.lihui.hilt.uitl.GlideUtil
+import com.rui.libray.ext.onClick
 
 object ImageBA {
     /**
@@ -13,4 +18,33 @@ object ImageBA {
     fun bindLevel(imageView: ImageView, userLevel: Int?) {
         imageView.setBackgroundResource(StarLevelEnum.match(userLevel))
     }
+
+    /**
+     * 头像改变
+     */
+    @JvmStatic
+    @BindingAdapter(value = ["userHeaderVm"])
+    fun bindUserHeader(imageView: ImageView?, userHeaderVm: UserVm?) {
+        val lifecycleOwner = imageView?.findViewTreeLifecycleOwner()
+        lifecycleOwner ?: return
+        userHeaderVm?.changeResult?.observe(lifecycleOwner, Observer {
+            GlideUtil.loadHeader(imageView, it)
+        })
+        imageView.onClick { userHeaderVm?.changeHeader() }
+    }
+
+    /**
+     * 用户等级改变
+     */
+    @JvmStatic
+    @BindingAdapter(value = ["userLevelVm"])
+    fun bindUserLevel(imageView: ImageView?, userLevelVm: UserVm?) {
+        val lifecycleOwner = imageView?.findViewTreeLifecycleOwner()
+        lifecycleOwner ?: return
+        userLevelVm?.level?.observe(lifecycleOwner, Observer {
+            imageView.setBackgroundResource(StarLevelEnum.match(it))
+        })
+        imageView.onClick { userLevelVm?.level?.value = (0..4).random() }
+    }
+
 }
